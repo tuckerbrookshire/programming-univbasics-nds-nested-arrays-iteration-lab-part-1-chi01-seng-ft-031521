4ee1a198aae30e9543204e72e32934ccc4f95381
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-19 17:20:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,9 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      # inside this loop, src[row_index][element_index] will access the current element
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
       element_index += 1
     end
     row_index += 1
